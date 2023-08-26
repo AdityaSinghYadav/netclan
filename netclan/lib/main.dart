225d9c25a41c4e93af6841d816b28c2990ab0586
AdityaSinghYadav <AdityaSinghYadav>
2023-08-26 16:40:43 +0530

    netclan

---
+++ netclan/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:netclan/home/home_page.dart';
+import 'package:netclan/page/explore_page.dart';
+import 'package:netclan/page/refine_page.dart';
+
+import 'package:device_preview/device_preview.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      isToolbarVisible: true,
+      tools: const [
+        ...DevicePreview.defaultTools,
+      ],
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      title: 'Netclan',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 6, 46, 51)),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
